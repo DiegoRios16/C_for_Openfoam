46c05a1f0c4ed675c1be29f8cf2e7eab4998e991
DiegoRios16 <diego.osito16@gmail.com>
2021-10-26 18:04:13 -0400

    Add KE Calculator

--- basicMath/add2_numbers.cpp
+++ basicMath/add2_numbers.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
 #include<string>
+#include<cmath>
 using namespace std;
 int main()
 {
 	float a,b;
 	string statement1, statement2, statement3;
-	statement1="We'll add 2 numbers: ";
-	statement2="Insert first number: ";
-	statement3="Insert 2nd number: ";
+	statement1="We'are going to calculate the KE=1/2mv^2";
+	statement2="Insert mass[kg]: ";
+	statement3="Insert velocity[m/s]: ";
 	cout<<statement1<<endl;
 	cout<<statement2;
 	cin>>a;
 	cout<<statement3;
 	cin>>b;
-	cout<<a+b<<endl;
+	cout<<0.5*a*pow(b,2)<<endl;
+	cout<<sqrt(2.0)<<endl;
+	cout<<pow(2.0, 0.5)<<endl;
 	return 0;
 }
